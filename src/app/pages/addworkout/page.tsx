24a400ea9c3c09248c89e1cd015ec08be2f4f202
SachinthanaK <kumaranayakages@gmail.com>
2024-08-16 23:59:02 +0530

    complete 1.4

--- src/app/pages/addworkout/page.tsx
+++ src/app/pages/addworkout/page.tsx
@@ -4,8 +4,43 @@ import React from "react";
 import "./addworkout.css";
 import { toast } from "react-toastify";
 
+interface Workout {
+  name: string;
+  description: string;
+  descriptionInMinutes: number;
+  exercises: Exercise[];
+  imageURL: string;
+  imageFile: File | null;
+}
+interface Exercise {
+  name: string;
+  description: string;
+  sets: number;
+  reps: number;
+  descriptionInMinutes: number;
+  exercises: Exercise[];
+  imageURL: string;
+  imageFile: File | null;
+}
 const page = () => {
-  return <div>page</div>;
+  const [workout, setWorkout] = React.useState<Workout>({
+    name: " ",
+    description: " ",
+    descriptionInMinutes: 0,
+    exercises: [],
+    imageURL: " ",
+    imageFile: null,
+  });
+
+  const [exercise, setExercise] = React.useState<Workout>({
+    name: " ",
+    description: " ",
+    sets: 0,
+    reps: 0,
+    imageURL: " ",
+    imageFile: null,
+  });
+  return <div></div>;
 };
 
 export default page;
